7cfb682dba6023ef9fe0dc9081d64bf01a039a0c
Joseph Goh <josephgoh7@gmail.com>
2022-08-17 12:01:37 +0800

    remove sensitive info

--- smart-door-lock-controller/src/secrets_default.h
+++ smart-door-lock-controller/src/secrets_default.h
@@ -1,9 +1,8 @@
 #ifndef _SECRETS_H
 #define _SECRETS_H
 
-// TODO: remove sensitive info from repo before releasing it
 // This file should be copied to secrets.h to add your own secrets
 
-#define WEBSERVER_DOMAIN "***REMOVED***"
+#define WEBSERVER_DOMAIN ""
 
 #endif  // _SECRETS_H
